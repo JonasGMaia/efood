63b6ca2d892430ef2cb3a8e02c5ed93988e5803c
Jonas Maia <jonasgmaia2@gmail.com>
2026-01-19 17:29:50 -0300

    form validation

--- src/components/Delivery/index.tsx
+++ src/components/Delivery/index.tsx
@@ -5,52 +5,145 @@ import { useDispatch, useSelector } from 'react-redux'
 import { RootReducer } from '../../store'
 import { setStep } from '../../store/reducers/cart'
 import { FormContainer, FormRow, BtnContainer } from './styles'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 const Delivery = () => {
   const { isOpen } = useSelector((state: RootReducer) => state.cart)
   const dispatch = useDispatch()
+  const formValid = useFormik({
+    initialValues: {
+      fullName: '',
+      adress: '',
+      city: '',
+      cep: '',
+      phoneNumber: '',
+      comment: ''
+    },
+    validationSchema: Yup.object({
+      fullName: Yup.string()
+        .min(5, 'O nome deve ter pelo menos 5 caracteres')
+        .required('O campo é obrigatório'),
+      adress: Yup.string().required('O campo é obrigatório'),
+      city: Yup.string().required('O campo é obrigatório'),
+      cep: Yup.string().min(9).max(9).required('O campo é obrigatório'),
+      phoneNumber: Yup.string()
+        .min(14)
+        .max(16)
+        .required('O campo é obrigatório')
+    }),
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  })
+
+  const getErrorMessage = (fieldName: string, message?: string) => {
+    const isAltered = fieldName in formValid.touched
+    const isInvalid = fieldName in formValid.errors
+
+    if (isAltered && isInvalid) return message
+    return ''
+  }
 
   return (
     <CartContainer className={isOpen ? 'is-open' : ''}>
       <Overlay style={{ position: 'absolute' }} />
       <Sidebar>
-        <FormContainer>
-          <h2>Entrega</h2>
-          <div>
-            <label htmlFor="fullName">Quem irá receber</label>
-            <input id="fullName" type="text" />
-          </div>
-          <div>
-            <label htmlFor="adress">Endereço</label>
-            <input id="adress" type="text" />
-          </div>
-          <div>
-            <label htmlFor="city">Cidade</label>
-            <input id="city" type="text" />
-          </div>
-          <FormRow>
+        <form onSubmit={formValid.handleSubmit}>
+          <FormContainer>
+            <h2>Entrega</h2>
             <div>
-              <label htmlFor="cep">CEP</label>
-              <input id="cep" type="text" />
+              <label htmlFor="fullName">Quem irá receber</label>
+              <input
+                id="fullName"
+                type="text"
+                name="fullName"
+                value={formValid.values.fullName}
+                onChange={formValid.handleChange}
+                onBlur={formValid.handleBlur}
+              />
+              <small>
+                {(getErrorMessage('fullName'), formValid.errors.fullName)}
+              </small>
             </div>
             <div>
-              <label htmlFor="phoneNumber">Número</label>
-              <input id="phoneNumber" type="text" />
+              <label htmlFor="adress">Endereço</label>
+              <input
+                id="adress"
+                type="text"
+                name="adress"
+                value={formValid.values.adress}
+                onChange={formValid.handleChange}
+                onBlur={formValid.handleBlur}
+              />
+              <small>
+                {(getErrorMessage('adress'), formValid.errors.adress)}
+              </small>
             </div>
-          </FormRow>
-          <div>
-            <label htmlFor="comment">Complemento (opcional)</label>
-            <input id="comment" type="text" />
-          </div>
-        </FormContainer>
-        <BtnContainer>
-          <button onClick={() => dispatch(setStep('payment'))}>
-            <AddButtonContainer>Continuar com pagamento</AddButtonContainer>
-          </button>
-          <button onClick={() => dispatch(setStep('cart'))}>
-            <AddButtonContainer>Voltar para o carrinho</AddButtonContainer>
-          </button>
-        </BtnContainer>
+            <div>
+              <label htmlFor="city">Cidade</label>
+              <input
+                id="city"
+                type="text"
+                name="city"
+                value={formValid.values.city}
+                onChange={formValid.handleChange}
+                onBlur={formValid.handleBlur}
+              />
+              <small>{(getErrorMessage('city'), formValid.errors.city)}</small>
+            </div>
+            <FormRow>
+              <div>
+                <label htmlFor="cep">CEP</label>
+                <input
+                  id="cep"
+                  type="text"
+                  name="cep"
+                  value={formValid.values.cep}
+                  onChange={formValid.handleChange}
+                  onBlur={formValid.handleBlur}
+                />
+                <small>{(getErrorMessage('cep'), formValid.errors.cep)}</small>
+              </div>
+              <div>
+                <label htmlFor="phoneNumber">Número</label>
+                <input
+                  id="phoneNumber"
+                  type="text"
+                  name="phoneNumber"
+                  value={formValid.values.phoneNumber}
+                  onChange={formValid.handleChange}
+                  onBlur={formValid.handleBlur}
+                />
+                <small>
+                  {
+                    (getErrorMessage('phoneNumber'),
+                    formValid.errors.phoneNumber)
+                  }
+                </small>
+              </div>
+            </FormRow>
+            <div>
+              <label htmlFor="comment">Complemento (opcional)</label>
+              <input
+                id="comment"
+                type="text"
+                name="comment"
+                value={formValid.values.comment}
+                onChange={formValid.handleChange}
+                onBlur={formValid.handleBlur}
+              />
+            </div>
+          </FormContainer>
+          <BtnContainer>
+            <button onClick={() => dispatch(setStep('payment'))}>
+              <AddButtonContainer>Continuar com pagamento</AddButtonContainer>
+            </button>
+            <button onClick={() => dispatch(setStep('cart'))}>
+              <AddButtonContainer>Voltar para o carrinho</AddButtonContainer>
+            </button>
+          </BtnContainer>
+        </form>
       </Sidebar>
     </CartContainer>
   )
--- src/components/Payment/index.tsx
+++ src/components/Payment/index.tsx
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { RootReducer } from '../../store'
 import { setStep, selectTotalNoCarrinho } from '../../store/reducers/cart'
 import { FormContainer, FormRow, BtnContainer } from '../Delivery/styles'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 const Payment = () => {
   const valorTotal = useSelector(selectTotalNoCarrinho)
@@ -16,48 +18,139 @@ const Payment = () => {
   }
   const { isOpen } = useSelector((state: RootReducer) => state.cart)
   const dispatch = useDispatch()
+  const formValid = useFormik({
+    initialValues: {
+      CardName: '',
+      CardNumber: '',
+      cvv: '',
+      validadeMes: '',
+      validadeAno: ''
+    },
+    validationSchema: Yup.object({
+      CardName: Yup.string()
+        .min(5, 'O nome deve ter pelo menos 5 caracteres')
+        .required('O campo é obrigatório'),
+      CardNumber: Yup.string()
+        .min(13)
+        .max(19)
+        .required('O campo é obrigatório'),
+      cvv: Yup.string()
+        .min(3)
+        .max(3, 'O nome deve ter pelo menos 3 caracteres')
+        .required('O campo é obrigatório'),
+      validadeMes: Yup.string().min(2).max(2).required('O campo é obrigatório'),
+      validadeAno: Yup.string().min(2).max(2).required('O campo é obrigatório')
+    }),
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  })
+
+  const getErrorMessage = (fieldName: string, message?: string) => {
+    const isAltered = fieldName in formValid.touched
+    const isInvalid = fieldName in formValid.errors
+
+    if (isAltered && isInvalid) return message
+    return ''
+  }
 
   return (
     <CartContainer className={isOpen ? 'is-open' : ''}>
       <Overlay style={{ position: 'absolute' }} />
       <Sidebar>
-        <FormContainer>
-          <h2>Pagamento - Valor a pagar {paraReal(valorTotal)}</h2>
-          <div>
-            <label htmlFor="CardName">Nome no Cartão</label>
-            <input id="CardName" type="text" />
-          </div>
-          <FormRow>
+        <form onSubmit={formValid.handleSubmit}>
+          <FormContainer>
+            <h2>Pagamento - Valor a pagar {paraReal(valorTotal)}</h2>
             <div>
-              <label htmlFor="CardNumber">Número do cartão</label>
-              <input id="CardNumber" type="text" />
+              <label htmlFor="CardName">Nome no Cartão</label>
+              <input
+                id="CardName"
+                type="text"
+                name="CardName"
+                value={formValid.values.CardName}
+                onChange={formValid.handleChange}
+                onBlur={formValid.handleBlur}
+              />
+              <small>
+                {(getErrorMessage('CardName'), formValid.errors.CardName)}
+              </small>
             </div>
-            <div>
-              <label htmlFor="cvv">CVV</label>
-              <input id="cvv" type="text" />
-            </div>
-          </FormRow>
-          <FormRow>
-            <div>
-              <label htmlFor="validadeMes">Mês de vencimento</label>
-              <input id="validade" type="text" />
-            </div>
-            <div>
-              <label htmlFor="validadeAno">Ano de vencimento</label>
-              <input id="validade" type="text" />
-            </div>
-          </FormRow>
-        </FormContainer>
-        <BtnContainer>
-          <button onClick={() => dispatch(setStep('confirmation'))}>
-            <AddButtonContainer>Finalizar pagamento</AddButtonContainer>
-          </button>
-          <button onClick={() => dispatch(setStep('delivery'))}>
-            <AddButtonContainer>
-              Voltar para edição de endereço
-            </AddButtonContainer>
-          </button>
-        </BtnContainer>
+            <FormRow>
+              <div>
+                <label htmlFor="CardNumber">Número do cartão</label>
+                <input
+                  id="CardNumber"
+                  type="text"
+                  name="CardNumber"
+                  value={formValid.values.CardNumber}
+                  onChange={formValid.handleChange}
+                  onBlur={formValid.handleBlur}
+                />
+                <small>
+                  {(getErrorMessage('CardNumber'), formValid.errors.CardNumber)}
+                </small>
+              </div>
+              <div>
+                <label htmlFor="cvv">CVV</label>
+                <input
+                  id="cvv"
+                  type="text"
+                  name="cvv"
+                  value={formValid.values.cvv}
+                  onChange={formValid.handleChange}
+                  onBlur={formValid.handleBlur}
+                />
+                <small>{(getErrorMessage('cvv'), formValid.errors.cvv)}</small>
+              </div>
+            </FormRow>
+            <FormRow>
+              <div>
+                <label htmlFor="validadeMes">Mês de vencimento</label>
+                <input
+                  id="validadeMes"
+                  type="text"
+                  name="validadeMes"
+                  value={formValid.values.validadeMes}
+                  onChange={formValid.handleChange}
+                  onBlur={formValid.handleBlur}
+                />
+                <small>
+                  {
+                    (getErrorMessage('validadeMes'),
+                    formValid.errors.validadeMes)
+                  }
+                </small>
+              </div>
+              <div>
+                <label htmlFor="validadeAno">Ano de vencimento</label>
+                <input
+                  id="validadeAno"
+                  type="text"
+                  name="validadeAno"
+                  value={formValid.values.validadeAno}
+                  onChange={formValid.handleChange}
+                  onBlur={formValid.handleBlur}
+                />
+                <small>
+                  {
+                    (getErrorMessage('validadeAno'),
+                    formValid.errors.validadeAno)
+                  }
+                </small>
+              </div>
+            </FormRow>
+          </FormContainer>
+          <BtnContainer>
+            <button onClick={() => dispatch(setStep('confirmation'))}>
+              <AddButtonContainer>Finalizar pagamento</AddButtonContainer>
+            </button>
+            <button onClick={() => dispatch(setStep('delivery'))}>
+              <AddButtonContainer>
+                Voltar para edição de endereço
+              </AddButtonContainer>
+            </button>
+          </BtnContainer>
+        </form>
       </Sidebar>
     </CartContainer>
   )
